3ffafeec2f5020f1a3e46e6de317a9712cb419b5
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-21 17:06:41 +0000

    Done.

---
+++ practicw.rb
@@ -0,0 +1,15 @@
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
+
+
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
